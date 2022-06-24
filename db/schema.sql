390fcb3a734b27902cdef71c9f28b61777fe0642
Nickolas Thompson <thompsonnickolas1@gmail.com>
2022-06-24 12:28:18 -0400

    fixed npm start, now tables arent pretty

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,48 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR (30)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL PRIMARY KEY,
     title VARCHAR (30) NOT NULL,
     salary DECIMAL(10) NOT NULL,
     department_id INT NOT NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT
-)
+);
+
+INSERT INTO departments (id, name)
+VALUES (1, "Sales"),
+       (2, "Finance"),
+       (3, "Engineering"),
+       (4, "Legal");
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (1, "Sales Lead", 64000, 1),
+       (2, "Salesperson", 53000, 1),
+       (3, "Lead Engineer", 100000, 3),
+       (4, "Software Engineer", 92000, 3),
+       (5, "Account Manager", 57000, 2),
+       (6, "Accountant", 52000, 2),
+       (7, "Legal Secretary", 60000, 4),
+       (8, "Lawyer", 50000, 4);
+
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Cason", "Smith", 1, 1),
+       (2, "Max", "Carbon", 1, 1),
+       (3, "Bryan", "Tabor", 3, 1),
+       (4, "Nickolas", "Thompson", 3, 1),
+       (5, "Marissa", "McBride", 2, 1),
+       (6, "Elias", "Morgan", 2, 1),
+       (7, "Katlynn", "Ennis", 4, 1),
+       (8, "Luke", "Hamlyn", 4, 1);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+require("console.table");
 
 const db = mysql.createConnection(
   {
@@ -12,6 +12,58 @@ const db = mysql.createConnection(
   console.log(`Connected to the company_db database.`)
 );
 
-db.query('SELECT * FROM company', function (err, results) {
-  console.log(results);
+const viewAllDepartments = () => {
+  db.query('SELECT * FROM departments', function (err, results) {
+    if (err) return console.error(err);
+    console.table(results);
+    return init();
+  });
+};
+
+const viewAllRoles = () => {
+  db.query('SELECT * FROM roles', function (err, results) {
+    if (err) return console.error(err);
+    console.table(results);
+    return init();
+  });
+};
+
+const viewAllEmployees = () => {
+  db.query('SELECT * FROM employees', function (err, results) {
+    if (err) return console.error(err);
+    console.table(results);
+    return init();
+  });
+}
+
+const init = () => {
+inquirer.prompt([
+  {
+    name: "query",
+    type: "list",
+    message: "What option would you like to select?",
+    choices: [
+      "View ALL Departments",
+      "View ALL Roles",
+      "View ALL Employees",
+    ]
+  }
+]).then((answers) => {
+  switch(answers.query) {
+    case "View ALL Departments": {
+      viewAllDepartments();
+      break;
+    }
+    case "View ALL Roles": {
+      viewAllRoles();
+      break;
+    }
+    case "View ALL Employees": {
+      viewAllEmployees();
+      break;
+    }
+  }
 });
+};
+
+init();
